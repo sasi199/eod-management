8b9772db36e0277642575308368808196037aebf
Elangai-wgs <elangaivendhan24@gmail.com>
2024-11-24 22:24:11 +0530

    syllabus

--- src/routes/syllabusRoute.js
+++ src/routes/syllabusRoute.js
@@ -5,10 +5,10 @@ const uploads = require('../middlewares/multer');
 const Router = express.Router();
 
 Router.route('/createSyllabus').post(uploads.single('uploadFile'),syllabusController.createSyllabus);
-Router.route('/createSyllabusAll').get(syllabusController.getSyllabusAll);
-Router.route('/createSyllabus/:_id').get(syllabusController.getSyllabusId);
-Router.route('/createSyllabus/:_id').put(uploads.single('uploadFile'),syllabusController.editSyllabus);
-Router.route('/createSyllabus?:_id').delete(syllabusController.deleteSyllabus);
+Router.route('/getSyllabusAll').get(syllabusController.getSyllabusAll);
+Router.route('/getSyllabus/:_id').get(syllabusController.getSyllabusId);
+Router.route('/editSyllabus/:_id').put(uploads.single('uploadFile'),syllabusController.editSyllabus);
+Router.route('/deleteSyllabus/:_id').delete(syllabusController.deleteSyllabus);
 
 
 
