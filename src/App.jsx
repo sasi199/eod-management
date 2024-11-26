0b6a4c917970b7cb089a43791a1928d35e849637
kavin-wgs <kavin@whyglobalservices.com>
2024-11-26 23:35:27 +0530

    KK title

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import "./App.css";
 import { Routers } from "./routes";
 import { Provider } from "react-redux";
 import { store } from "./Redux/Store";
+import { useEffect } from "react";
 
 function App() {
+  const location = useLocation();
+  const url = location.pathname.split("/");
+  const panel = url[1];
+  useEffect(() => {
+    panel === "sidebar" ? (document.title = "Super Admin") : "";
+    panel === "trainersidebar" ? (document.title = "Trainer") : "";
+    panel === "traineesidebar" ? (document.title = "Student") : "";
+  }, []);
   return (
     <>
-    <Provider store={store}>
-      <BrowserRouter>
+      <Provider store={store}>
         <Routers />
-      </BrowserRouter>
       </Provider>
     </>
   );
