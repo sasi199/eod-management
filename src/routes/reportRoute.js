e9f91b2bf21a1b0e772519dd95c30980bd8623df
Elangai-wgs <elangaivendhan24@gmail.com>
2024-12-02 14:56:11 +0530

    report change

--- src/routes/reportRoute.js
+++ src/routes/reportRoute.js
@@ -6,7 +6,7 @@ const Router = express.Router()
 
 Router.route('/createReport').post(reportController.createReport);
 Router.route('/getReportAll').get(reportController.getReportAll);
-Router.route('/getReport/:_id').get(reportController.getReportId);
+Router.route('/getReportId/:_id').get(reportController.getReportId);
 Router.route('/editReport/:_id').put(reportController.editReport);
 Router.route('/deleteReport/:_id').delete(reportController.deleteReport);
 
