1449416bb68cb8acc0e7b37e7b692174f8149891
Elangai-wgs <elangaivendhan24@gmail.com>
2024-12-05 12:39:09 +0530

    eod change

--- src/routes/eodRoute.js
+++ src/routes/eodRoute.js
@@ -7,5 +7,9 @@ const { verifyAuthToken } = require('../middlewares/jwt.config');
 const Router = express.Router();
 
 Router.route('/createEod').post(verifyAuthToken,uploads.fields([{ name: 'uploadFile', maxCount: 10 },]),eodController.createEod);
+Router.route('/getEodAll').get(eodController.getEodAll);
+Router.route('/getEodId').get(eodController.getEodById);
+Router.route('/editEod').get(eodController.editEod);
+Router.route('/deleteEod').get(eodController.deleteEod);
 
 module.exports = Router;
--- src/services/eodServices.js
+++ src/services/eodServices.js
@@ -60,3 +60,68 @@ exports.createEod = async(req)=>{
 
     return newEod;
 }
+
+exports.getEodAll = async () => {
+    const eods = await EodModel.find().populate("userName").populate("project");
+    return eods;
+};
+
+
+exports.getEodById = async (id) => {
+    const eod = await EodModel.findById(id).populate("userName").populate("project");
+    if (!eod) {
+        throw new ApiError(httpStatus.NOT_FOUND, { message: "EOD not found" });
+    }
+    return eod;
+};
+
+
+exports.editEod = async(req)=>{
+    const { project, department,description, links} = req.body
+    const { accountId } = req;
+    const { _id } = req.params;
+
+    const eod = await EodModel.findById(id);
+    if (!eod) {
+        throw new ApiError(httpStatus.NOT_FOUND, { message: "EOD not found" });
+    }
+
+    if (project) {
+        const projects = await ProjectModel.findOne({ projectName: project });
+        if (!projects) {
+            throw new ApiError(httpStatus.BAD_REQUEST, { message: "Project not found" });
+        }
+        eod.project = project;
+    }
+
+    if (req.files && req.files.uploadFile && req.files.uploadFile.length > 0) {
+        const uploadPromises = req.files.uploadFile.map(async (file) => {
+            const fileExtension = file.originalname.split(".").pop();
+            const fileName = `${Date.now()}_${Math.random().toString(36).substring(2)}.${fileExtension}`;
+            return await uploadCloud(`Eod/${fileName}`, file);
+        });
+
+        const uploadFiles = await Promise.all(uploadPromises);
+        eod.uploadFile.push(...uploadFiles);
+    }
+
+    if (department) eod.department = department;
+    if (description) eod.description = description;
+    if (links) {
+        eod.link = Array.isArray(links) ? links : JSON.parse(links);
+    }
+
+    await eod.save();
+    return eod;
+}
+
+
+
+exports.deleteEod = async () => {
+    const {_id} = req.params;
+    const eod = await EodModel.findByIdAndDelete(_id);
+    if (!eod) {
+        throw new ApiError(httpStatus.NOT_FOUND, { message: "EOD not found" });
+    }
+    return { message: "EOD deleted successfully" };
+};
